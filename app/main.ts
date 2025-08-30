68bef33507710c89bf1ecf930c06de3fc78cbf94
vicky <vickykumarsunilja@gmail.com>
2025-08-30 09:37:38 +0530

    Implemented the request target extraction

--- app/main.ts
+++ app/main.ts
@@ -5,10 +5,50 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("close", () => {
+  socket.setEncoding("utf-8");
+
+  socket.on("data", (data: any) => {
+    const requestLine = data.split("\r\n")[0]; // Get the request line
+    const parts = requestLine.split(" "); // Split by spaces
+
+    // Check if it's a GET request
+    const method = parts[0];
+    const path = parts[1]; // request target - origin form
+    const httpVersion = parts[2];
+
+    console.log("Method:", method);
+    console.log("Request target:", path);
+    console.log("HTTP version:", httpVersion);
+
+    let responseBody = "";
+
+    if (method === "GET") {
+      if (path === "/" || path === "/index.html") {
+        const body = "Hello from index!";
+        responseBody =
+          `HTTP/1.1 200 OK\r\n` +
+          `Content-Length: ${body.length}\r\n` +
+          `Content-Type: text/plain\r\n` +
+          `\r\n` +
+          body;
+      } else {
+        const body = "404 Not Found";
+        responseBody =
+          `HTTP/1.1 404 Not Found\r\n` +
+          `Content-Length: ${body.length}\r\n` +
+          `Content-Type: text/plain\r\n` +
+          `\r\n` +
+          body;
+      }
+    }
+
+    socket.write(responseBody);
     socket.end();
   });
+  // socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  // socket.on("close", () => {
+  //   socket.end();
+  // });
 });
 //
 server.listen(4221, "localhost");
